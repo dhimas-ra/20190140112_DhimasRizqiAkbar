bcc4985ecd77e051496a8ccddd864f72a9f12c90
Dhimas <dhimas.ra48@gmail.com>
2021-04-03 22:45:18 +0700

    mant

---
+++ Praktikum javascript/app.js
@@ -0,0 +1,14 @@
+/*
+//alert dari app.js
+alert('ini alert dari app.js')
+
+//Confirm dari app.js
+confirm('ini confirm dari app.js')
+
+//Console.log
+console.log('ini console.log dari app.js)
+*/
+//Tanggal
+Date()
+var Tanggal = new Date()
+document.getElementById('Tanggal').innerHTML = Tanggal;
